c3ce5387aabaf23974d5522ea6dcf38476e3680c
Mournx <114749264+Mournx@users.noreply.github.com>
2024-10-13 19:51:39 +0800

    Configure Windows Window and GLFW for Render.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,13 +1,16 @@
+#include "hzpch.h"
 #include "Application.h"
 
-#include "Log.h"
-#include "Events/ApplicationEvent.h"
+#include "GLFW/glfw3.h"
 
 namespace Hazel
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
@@ -15,11 +18,28 @@ namespace Hazel
 
 	}
 
-	void Application::Run()
+	void Application::OnEvent(Event& e)
 	{
-		WindowResizeEvent e(1200, 720);
-		HZ_TRACE(e.ToString());
-		while (true);
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		HZ_CORE_TRACE("{0}", e.ToString());
 	}
 
+	void Application::Run()
+	{
+		
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
 }
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include"Core.h"
-#include"Events/Event.h"
+#include "Core.h"
+#include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+#include "Window.h"
 
 namespace Hazel
 {
@@ -12,6 +14,12 @@ namespace Hazel
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	//To be defined in CLIENT
