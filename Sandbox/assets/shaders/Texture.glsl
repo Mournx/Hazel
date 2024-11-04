9f018c932b74b4e7dd87196a8aaf86e5f8d5007c
Mournx <114749264+Mournx@users.noreply.github.com>
2024-11-04 19:37:32 +0800

    Update Renderer2D API.

--- Sandbox/assets/shaders/Texture.glsl
+++ Sandbox/assets/shaders/Texture.glsl
@@ -23,11 +23,12 @@ void main()
 layout(location = 0) out vec4 color;
 
 uniform sampler2D u_Texture;
+uniform float u_TilingFactor;
 uniform vec4 u_Color;
 
 in vec2 v_TexCoord;
 
 void main()
 {
-	color = texture(u_Texture, v_TexCoord * 10.0) * u_Color;
+	color = texture(u_Texture, v_TexCoord * u_TilingFactor) * u_Color;
 }
