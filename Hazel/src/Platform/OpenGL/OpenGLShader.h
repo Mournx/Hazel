57554306ad0956ec6b1b15db4f789f64b207953c
Mournx <114749264+Mournx@users.noreply.github.com>
2024-11-12 21:19:10 +0800

    Added textures to batch renderer.

--- Hazel/src/Platform/OpenGL/OpenGLShader.h
+++ Hazel/src/Platform/OpenGL/OpenGLShader.h
@@ -18,6 +18,7 @@ namespace Hazel {
 		virtual void Unbind() const override;
 
 		virtual void SetInt(const std::string& name, int value) override;
+		virtual void SetIntArray(const std::string& name, int* values, uint32_t count) override;
 		virtual void SetFloat(const std::string& name, float value) override;
 		virtual void SetFloat3(const std::string& name, const glm::vec3& value) override;
 		virtual void SetFloat4(const std::string& name, const glm::vec4& value) override;
@@ -26,6 +27,7 @@ namespace Hazel {
 		virtual const std::string& GetName() const override { return m_Name; }
 
 		void UploadUniformInt(const std::string& name, int value) const;
+		void UploadUniformIntArray(const std::string& name, int* values, uint32_t count) const;
 
 		void UploadUniformFloat(const std::string& name, float value) const;
 		void UploadUniformFloat2(const std::string& name, const glm::vec2& value) const;
